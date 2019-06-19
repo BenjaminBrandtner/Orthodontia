6738f2ec7da3afdbfb97123ad3b616efe6aac2fb
Benjamin Brandtner <37274870+BenjaminBrandtner@users.noreply.github.com>
2019-06-19 11:33:33 +0200

    Don't reset options every time the extension is updated

--- background.js
+++ background.js
@@ -1,13 +1,20 @@
 chrome.runtime.onInstalled.addListener(() =>
 {
-	var orthodontiaOptions =
+	chrome.storage.sync.get(["orthodontiaOptions"], result =>
+	{
+		if (result.orthodontiaOptions === null || result.orthodontiaOptions === undefined)
 		{
-			debug: true,
-			debugInfo: {},
-			userClasses: ".w3-code",
-			preferredBraceStyle: "NEXTLINE",
-			automaticConversion: true
-		};
 
-	chrome.storage.sync.set({"orthodontiaOptions": orthodontiaOptions});
+			var orthodontiaOptions =
+				{
+					debug: true,
+					debugInfo: {},
+					userClasses: ".w3-code",
+					preferredBraceStyle: "NEXTLINE",
+					automaticConversion: true
+				};
+
+			chrome.storage.sync.set({"orthodontiaOptions": orthodontiaOptions});
+		}
+	});
 });
