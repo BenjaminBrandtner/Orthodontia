4b21dc0a04c1ce1d7a7e8c00574d0ed543494079
Benjamin Brandtner <37274870+BenjaminBrandtner@users.noreply.github.com>
2019-09-09 09:45:10 +0200

    Now using chai assertion library

--- karma.conf.js
+++ karma.conf.js
@@ -55,6 +55,8 @@ module.exports = function(config) {
 
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
+	  
+	autoWatchBatchDelay: 500,
 
 
     // start these browsers
--- test/identifying.test.js
+++ test/identifying.test.js
@@ -1,12 +1,14 @@
-const assert = require("assert"); //TODO: replace assert with real assertion library
+const chai = require("chai");
+const expect = chai.expect;
 const orthodontia = require("../src/orthodontia.js");
 
-//Destructuring the exports of orthodontia into global variables, so instead of writng orthodontia.foo() everywhere you can just write foo() instead.
+//Destructuring the exports of orthodontia into global variables, so instead of writing orthodontia.foo() everywhere you can just write foo() instead.
 for (let key in orthodontia)
 {
 	eval("var {" + key + "} = orthodontia;");
 }
-//TODO: The preparation abvove is the same for every testing file, find out how to do it before each test suite
+
+//Preparation done.
 
 describe("Identifying", () =>
 {
@@ -21,15 +23,14 @@ describe("Identifying", () =>
 	it("identifies NONE correctly", () =>
 	{
 		let bracestyle = identifyBraceStyle(element);
-		assert.equal(bracestyle, "NONE");
-
+		expect(bracestyle).to.equal("NONE");
 	});
 	it("identifies Array Initalisation as NONE", () =>
 	{
 		element.innerText = "int zahlen[] = { 17, 0, 3 };\n foo();";
 
 		let bracestyle = identifyBraceStyle(element);
-		assert.equal(bracestyle, "NONE");
+		expect(bracestyle).to.equal("NONE");
 
 	});
 	it("identifies SAMELINE correctly", () =>
@@ -37,14 +38,14 @@ describe("Identifying", () =>
 		element.innerText = "hello {\n world }";
 
 		let bracestyle = identifyBraceStyle(element);
-		assert.equal(bracestyle, "SAMELINE");
+		expect(bracestyle).to.equal("SAMELINE");
 	});
 	it("identifies NEXTLINE correctly", () =>
 	{
 		element.innerText = "hello\n {\n world }";
 
 		let bracestyle = identifyBraceStyle(element);
-		assert.equal(bracestyle, "NEXTLINE");
+		expect(bracestyle).to.equal("NEXTLINE");
 	});
 
 	it("identifies SAMELINE correctly, ignoring Array Initalisation", () =>
@@ -52,14 +53,14 @@ describe("Identifying", () =>
 		element.innerText = "int zahlen[] = { 17, 0, 3 };\nhello {\n world }";
 
 		let bracestyle = identifyBraceStyle(element);
-		assert.equal(bracestyle, "SAMELINE");
+		expect(bracestyle).to.equal("SAMELINE");
 	});
 	it("identifies NEXTLINE correctly, ignoring Array Initalisation", () =>
 	{
 		element.innerText = "int zahlen[] = { 17, 0, 3 };\nhello\n {\n world }";
 
 		let bracestyle = identifyBraceStyle(element);
-		assert.equal(bracestyle, "NEXTLINE");
+		expect(bracestyle).to.equal("NEXTLINE");
 	});
 
 	it("identifies UNCLEAR correctly", () =>
@@ -75,7 +76,7 @@ byebye
 `;
 
 		let bracestyle = identifyBraceStyle(element);
-		assert.equal(bracestyle, "UNCLEAR");
+		expect(bracestyle).to.equal("UNCLEAR");
 	});
 
 });
